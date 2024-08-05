1bbe8cee0acf20ce66a6cf668d3c2e3394e066a8
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-05 11:31:09 -0300

    AULA USESTATE

---
+++ aula/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NumeroAleatorio from './assets/NumeroAleatorio'
+import Contador from './assets/Contador'
+
+
+function App() {
+  return (
+    <>
+    <Contador/>
+    <NumeroAleatorio/>
+    </>
+  )
+}
+
+export default App
---
+++ aula/src/assets/Contador.jsx
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+
+
+export default function Contador(){
+    const [textoBotao,settextoBotao] = useState('Rebeca')
+
+    function handleClick(){
+     settextoBotao( textoBotao === "Rebeca" ? "Ouro": "Rebeca")
+    }
+   
+     return (
+       <>
+
+       <button onClick={handleClick}>{textoBotao}</button>
+       </>
+     )
+}
